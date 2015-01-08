262b1d979aa3700e7758486036634414b62435fb
Aistis <aistis8624@gmail.com>
2015-01-08 21:29:43 +0100

    Style

--- RpcHttpBindingValidator.php
+++ RpcHttpBindingValidator.php
@@ -6,6 +6,11 @@
 class RpcHttpBindingValidator
 {
     /**
+     * XML schema namespace URL for envelope tag.
+     */
+    const NS_XML_ENVELOPE = 'http://schemas.xmlsoap.org/soap/enveloper';
+
+    /**
      * Validate HTTP request headers using $_SERVER global variable.
      *
      * Note: HTTP header parameters names are case-insensitive.
@@ -15,7 +20,8 @@ class RpcHttpBindingValidator
     public static function validate()
     {
         // Check if request method is GET or POST
-        if (in_array($_SERVER['REQUEST_METHOD'], array('GET', 'POST'), true)) {
+        if (isset($_SERVER['REQUEST_METHOD']) &&
+            in_array($_SERVER['REQUEST_METHOD'], array('GET', 'POST'), true)) {
 
             // Check if SOAPAction field exists in request header and it is not empty
             if (isset($_SERVER['HTTP_SOAPACTION']) && !empty($_SERVER['HTTP_SOAPACTION'])) {
@@ -28,9 +34,18 @@ class RpcHttpBindingValidator
                     return false;
                 }
 
+                // Namespaces checking
+                $namespace = array_search(self::NS_XML_ENVELOPE, $xml->getNamespaces(), true);
+
                 $methodName = htmlspecialchars($_SERVER['HTTP_SOAPACTION']);
 
-                $xPath = "//SOAP-ENV:Envelope/SOAP-ENV:Body/*[local-name()='{$methodName}']";
+                // Check if namespace in xml was not found
+                if (empty($namespace)) {
+                    return false;
+                }
+
+                $xPath = "//{$namespace}:Envelope/{$namespace}:Body/*[local-name()='{$methodName}']";
+
 
                 $xmlElements = $xml->xpath($xPath);
 
